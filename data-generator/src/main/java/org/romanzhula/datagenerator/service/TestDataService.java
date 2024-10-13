67fae5b75d6658a0e500a1d9a239beeeeaecdcdd
RoMANzhula <roman1985al@ukr.net>
2024-10-13 20:48:20 +0300

    Modul data-generator - add TestDataService

---
+++ data-generator/src/main/java/org/romanzhula/datagenerator/service/TestDataService.java
@@ -0,0 +1,10 @@
+package org.romanzhula.datagenerator.service;
+
+import org.romanzhula.datagenerator.model.DataTestOptions;
+
+
+public interface TestDataService {
+
+    void sendMessages(DataTestOptions dataTestOptions);
+
+}
