90afeb772188ad2a8b66be4dee1f7764dd875c62
RoMANzhula <roman1985al@ukr.net>
2024-10-13 20:49:43 +0300

    Modul data-generator - add TextXPath

---
+++ data-generator/src/main/java/org/romanzhula/datagenerator/configuration/TextXPath.java
@@ -0,0 +1,21 @@
+package org.romanzhula.datagenerator.configuration;
+
+import com.jcabi.xml.XML;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public final class TextXPath {
+
+    private final XML xml;
+    private final String node;
+
+    @Override
+    public String toString() {
+        return this.xml.nodes(node)
+                .get(0)
+                .xpath("text()")
+                .get(0)
+        ;
+    }
+
+}
