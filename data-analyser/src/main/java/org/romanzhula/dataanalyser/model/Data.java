a82c2adef4963810d58fa25f516e35b4bfb006ca
RoMANzhula <roman1985al@ukr.net>
2024-10-15 23:33:52 +0300

    Up Data - add JPA and jakarta.persistence annotation

--- data-analyser/src/main/java/org/romanzhula/dataanalyser/model/Data.java
+++ data-analyser/src/main/java/org/romanzhula/dataanalyser/model/Data.java
@@ -1,5 +1,6 @@
 package org.romanzhula.dataanalyser.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,15 +9,25 @@ import org.romanzhula.dataanalyser.model.enums.MeasurementType;
 
 import java.time.LocalDateTime;
 
+
+@Entity
+@Table(name = "data")
 @NoArgsConstructor
 @Getter
 @Setter
 @ToString
 public class Data {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private Long sensorId;
     private LocalDateTime timestamp;
     private Double measurement;
+
+    @Column(name = "meas_type")
+    @Enumerated(value = EnumType.STRING)
     private MeasurementType measurementType;
 
 }
