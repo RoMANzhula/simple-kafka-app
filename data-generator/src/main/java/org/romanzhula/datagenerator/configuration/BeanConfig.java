fc618bffebf72a298aeac582a094de7ca3e97420
RoMANzhula <roman1985al@ukr.net>
2024-10-17 22:23:30 +0300

    Up BeanConfig - change to absolute path

--- data-generator/src/main/java/org/romanzhula/datagenerator/configuration/BeanConfig.java
+++ data-generator/src/main/java/org/romanzhula/datagenerator/configuration/BeanConfig.java
@@ -5,8 +5,7 @@ import com.jcabi.xml.XMLDocument;
 import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.io.File;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class BeanConfig {
@@ -14,7 +13,8 @@ public class BeanConfig {
     @SneakyThrows //without exception handling in this layer
     @Bean
     public XML producerXML() {
-        return new XMLDocument(new File("src/main/resources/kafka/producer.xml"));
+        ClassPathResource resource = new ClassPathResource("kafka/producer.xml");
+        return new XMLDocument(resource.getInputStream());
     }
 
 }
