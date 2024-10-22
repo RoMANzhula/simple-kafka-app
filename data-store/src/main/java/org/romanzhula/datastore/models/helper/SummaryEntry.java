8fe60d410b8d47da6bbd2960446f2334d5a8bd05
RoMANzhula <roman1985al@ukr.net>
2024-10-22 22:54:10 +0300

    Add SummaryEntry class as helper - data-store modul

---
+++ data-store/src/main/java/org/romanzhula/datastore/models/helper/SummaryEntry.java
@@ -0,0 +1,19 @@
+package org.romanzhula.datastore.models.helper;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.romanzhula.datastore.models.enums.SummaryType;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class SummaryEntry {
+
+    private SummaryType summaryType;
+    private Double value;
+
+}
