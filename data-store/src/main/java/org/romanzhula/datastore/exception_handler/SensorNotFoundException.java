f9ef838dcaa321001d934defa13c09e2885fde59
RoMANzhula <roman1985al@ukr.net>
2024-10-23 23:17:00 +0300

    Add SensorNotFoundException - data-store modul

---
+++ data-store/src/main/java/org/romanzhula/datastore/exception_handler/SensorNotFoundException.java
@@ -0,0 +1,9 @@
+package org.romanzhula.datastore.exception_handler;
+
+public class SensorNotFoundException extends RuntimeException {
+
+//    public SensorNotFoundException(String message) {
+//        super(message);
+//    }
+
+}
