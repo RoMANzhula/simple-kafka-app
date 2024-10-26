50bbc504638b8e7c8b1e7648a3640771b32388bc
RoMANzhula <roman1985al@ukr.net>
2024-10-26 22:43:59 +0300

    Up SummaryRepositoryImpl - add handle() method - data-store modul

--- data-store/src/main/java/org/romanzhula/datastore/repositories/implementations/SummaryRepositoryImpl.java
+++ data-store/src/main/java/org/romanzhula/datastore/repositories/implementations/SummaryRepositoryImpl.java
@@ -3,6 +3,7 @@ package org.romanzhula.datastore.repositories.implementations;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.common.protocol.types.Field;
 import org.romanzhula.datastore.configurations.RedisSchema;
+import org.romanzhula.datastore.models.Data;
 import org.romanzhula.datastore.models.Summary;
 import org.romanzhula.datastore.models.enums.MeasurementType;
 import org.romanzhula.datastore.models.enums.SummaryType;
@@ -89,10 +90,101 @@ public class SummaryRepositoryImpl implements SummaryRepository {
                     summaryEntry.setValue(Double.parseDouble(value));
                 }
 
+                String counter = jedis.hget(
+                        RedisSchema.summaryKey(sensorId, measurementType),
+                        "counter"
+                );
+
+                if (counter != null) {
+                    summaryEntry.setCounter(Long.parseLong(counter));
+                }
+
                 summary.addValue(measurementType, summaryEntry);
             }
         }
 
         return Optional.of(summary);
     }
+
+    @Override
+    public void handle(Data data) {
+        try(Jedis jedis = jedisPool.getResource()) {
+            if (!jedis.sismember(RedisSchema.sensorKeys(), String.valueOf(data.getSensorId()))) {
+                jedis.sadd(RedisSchema.sensorKeys(), String.valueOf(data.getSensorId()));
+            }
+
+            updateMinValue(data, jedis);
+            updateMaxValue(data, jedis);
+            updateSumAndAvgValues(data, jedis);
+        }
+    }
+
+
+    private void updateMinValue(Data data, Jedis jedis) {
+        String key = RedisSchema.summaryKey(data.getSensorId(), data.getMeasurementType());
+        String summaryType = SummaryType.MIN.name().toLowerCase();
+
+        String value = jedis.hget(key, summaryType);
+
+        boolean measurementLessValue = data.getMeasurement() < Double.parseDouble(value);
+        String measurement = String.valueOf(data.getMeasurement());
+
+        if (measurementLessValue) {
+            jedis.hset(key, summaryType, measurement);
+        }
+    }
+
+    private void updateMaxValue(Data data, Jedis jedis) {
+        String key = RedisSchema.summaryKey(data.getSensorId(), data.getMeasurementType());
+        String summaryType = SummaryType.MAX.name().toLowerCase();
+
+        String value = jedis.hget(key, summaryType);
+
+        boolean measurementLessValue = data.getMeasurement() > Double.parseDouble(value);
+        String measurement = String.valueOf(data.getMeasurement());
+
+        if (measurementLessValue) {
+            jedis.hset(key, summaryType, measurement);
+        }
+    }
+
+    private void updateSumAndAvgValues(Data data, Jedis jedis) {
+        updateSumValue(data, jedis);
+
+        String key = RedisSchema.summaryKey(data.getSensorId(), data.getMeasurementType());
+        String summaryTypeSum = SummaryType.SUM.name().toLowerCase();
+        String summaryTypeAvg = SummaryType.AVG.name().toLowerCase();
+
+
+        String counter = jedis.hget(key, "counter");
+
+        if (counter == null) {
+            counter = String.valueOf(jedis.hset(key, "counter", "1"));
+        } else {
+            counter = String.valueOf(jedis.hincrBy(key, "counter", 1));
+        }
+
+        String sum = jedis.hget(key, summaryTypeSum);
+
+        String average = String.valueOf(Double.parseDouble(sum) / Double.parseDouble(counter));
+
+        jedis.hset(key, summaryTypeAvg, average);
+
+    }
+
+    private void updateSumValue(Data data, Jedis jedis) {
+        String key = RedisSchema.summaryKey(data.getSensorId(), data.getMeasurementType());
+        String summaryType = SummaryType.SUM.name().toLowerCase();
+
+        String value = jedis.hget(key, summaryType);
+
+        String measurement = String.valueOf(data.getMeasurement());
+
+        if (value == null) { // for first income
+            jedis.hset(key, summaryType, measurement);
+        } else {
+            jedis.hincrByFloat(key, summaryType, data.getMeasurement());
+        }
+    }
+
 }
