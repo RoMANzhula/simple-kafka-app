8c7e57209a4e3e6ba16c15f01fdddf3dffd1d19e
RoMANzhula <roman1985al@ukr.net>
2024-10-29 23:37:16 +0200

    Up migration file - change structure

--- data-analyser/src/main/resources/db/changelog/change_sets/001_create_table_data.sql
+++
@@ -1,8 +0,0 @@
-create table data
-    (
-        id          BIGSERIAL PRIMARY KEY ,
-        sensor_id   BIGINT NOT NULL,
-        timestamp   TIMESTAMP NOT NULL,
-        measurement FLOAT NOT NULL,
-        meas_type   VARCHAR NOT NULL
-    );
